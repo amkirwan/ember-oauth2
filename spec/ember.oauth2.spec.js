471c04c777506509efc5792fab39b8194b2a2c5d
Anthony Kirwan <amkirwan@gmail.com>
2013-01-11 17:55:08 -0600

    added Ember.OAuth2 object

--- spec/ember.oauth2.spec.js
+++ spec/ember.oauth2.spec.js
@@ -1,1 +1,23 @@
+describe("ember-oauth2", function() {
 
+  window.App = Ember.Application.create({
+    NAME: 'test-app'    
+  });
+
+  // beforeEach(function() {
+  //   App.oauth = Ember.OAuth2.create();
+  // });
+
+  describe("Ember OAuth2 Object", function() {
+    it("should create a OAuth2 object", function() {
+      expect(Ember.OAuth2).toBeDefined();
+    });
+  });
+
+  describe("Ember OAuth2", function() {
+    it("should create a configuration object for Ember.OAuth2", function() {
+      expect(Ember.OAuth2.configure).toBeDefined();
+    });
+  });
+
+});
---
+++ src/ember.oauth2.js
@@ -0,0 +1,9 @@
+(function() {
+  if (Ember.OAuth2 == undefined) {
+    Ember.OAuth2 = Ember.Object.extend();
+  }
+
+  Ember.OAuth2.configure = {}
+
+
+})(this);
