22c2d5f4797eaa8453ce5f9fee6f736170c531ed
Anthony Kirwan <amkirwan@gmail.com>
2013-01-11 23:48:47 -0600

    generate authUri

--- src/ember.oauth2.js
+++ src/ember.oauth2.js
@@ -1,9 +1,23 @@
 (function() {
   if (Ember.OAuth2 == undefined) {
-    Ember.OAuth2 = Ember.Object.extend();
+    Ember.OAuth2 = Ember.Object.extend({
+      calcState: function() {
+        var rand = Math.random();
+        var dateTime = new Date().getTime();
+        return rand * dateTime;
+      },
+
+      authUri: function() {
+        var url = this.authBaseUri + '?response_type=token' + '&redirect_uri=' + this.redirectUri + '&client_id=' + this.clientId; 
+        if (this.state) url += '&state=' + this.state;
+        if (this.scope) url += '&scope=' + this.scope;
+        return url;
+      }  
+    });
   }
 
-  Ember.OAuth2.configure = {}
+  Ember.OAuth2.config = {}
+
 
 
 })(this);
