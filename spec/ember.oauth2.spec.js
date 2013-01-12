bb672de7d860e4a51ac6eee4489c0e8be51038c0
Anthony Kirwan <amkirwan@gmail.com>
2013-01-11 18:02:56 -0600

    renamed describe blocks

--- spec/ember.oauth2.spec.js
+++ spec/ember.oauth2.spec.js
@@ -8,13 +8,11 @@ describe("ember-oauth2", function() {
   //   App.oauth = Ember.OAuth2.create();
   // });
 
-  describe("Ember OAuth2 Object", function() {
+  describe("Create Namespaces and configure object", function() {
     it("should create a OAuth2 object", function() {
       expect(Ember.OAuth2).toBeDefined();
     });
-  });
 
-  describe("Ember OAuth2", function() {
     it("should create a configuration object for Ember.OAuth2", function() {
       expect(Ember.OAuth2.configure).toBeDefined();
     });
