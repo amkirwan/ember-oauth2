3a665470045704d5503bd277a5db5aa49523f200
Anthony Kirwan <amkirwan@gmail.com>
2013-01-12 23:43:42 -0600

    moved before filter to setup authUri

--- spec/ember.oauth2.spec.js
+++ spec/ember.oauth2.spec.js
@@ -4,6 +4,30 @@ describe("ember-oauth2", function() {
     NAME: 'test-app'    
   });
 
+  var authorizeUri;
+  beforeEach(function() {
+    var authBaseUri = 'https://foobar.dev/oauth/authorize';
+    var redirectUri = 'https://qux.dev/oauth/callback';
+    var clientId = '12345';
+    var scope = 'public';
+    var state = '6789';
+    Ember.OAuth2.config = {
+      test_auth: {
+        clientId: clientId,
+        authBaseUri: authBaseUri,
+        redirectUri: redirectUri,
+        scope: scope,
+        state: state
+      }
+    };
+    App.oauth = Ember.OAuth2.create(Ember.OAuth2.config.test_auth);
+    authorizeUri = authBaseUri + '?response_type=token' + '&redirect_uri=' + redirectUri + '&client_id=' + clientId + '&state=' + state + '&scope=' + scope;
+  });
+
+  afterEach(function() {
+    authorizeUri = null;
+  });
+
   describe("Create Namespaces and configure object", function() {
     it("should create a OAuth2 object", function() {
       expect(Ember.OAuth2).toBeDefined();
@@ -14,32 +38,8 @@ describe("ember-oauth2", function() {
     });
   });
 
-  describe("It should setup the authorization url", function() {
-    var authorizeUri;
-    beforeEach(function() {
-      var authBaseUri = 'https://foobar.dev/oauth/authorize';
-      var redirectUri = 'https://qux.dev/oauth/callback';
-      var clientId = '12345';
-      var scope = 'public';
-      var state = '6789';
-      Ember.OAuth2.config = {
-        test_auth: {
-          clientId: clientId,
-          authBaseUri: authBaseUri,
-          redirectUri: redirectUri,
-          scope: scope,
-          state: state
-        }
-      };
-      App.oauth = Ember.OAuth2.create(Ember.OAuth2.config.test_auth);
-      authorizeUri = authBaseUri + '?response_type=token' + '&redirect_uri=' + redirectUri + '&client_id=' + clientId + '&state=' + state + '&scope=' + scope;
-    });
-
-    afterEach(function() {
-      authorizeUri = null;
-    });
-
-    it("should setup the url", function() {
+  describe("Generate OAuth2 providers url", function() {
+        it("should create the url with the options", function() {
       expect(App.oauth.authUri()).toEqual(authorizeUri)
     });
   });
