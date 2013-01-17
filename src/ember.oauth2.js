bb871a7f5ad9370db54d6aef97caf43416dab900
Anthony Kirwan <amkirwan@gmail.com>
2013-01-17 00:10:13 -0600

    fixed problem with generating uuid

--- src/ember.oauth2.js
+++ src/ember.oauth2.js
@@ -9,7 +9,7 @@
       },
 
       uuid: function() {
-        'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
               var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
                   return v.toString(16);
         });
@@ -130,7 +130,7 @@
        */ 
       getState: function(state) {
         var obj = JSON.parse(window.localStorage.getItem('state-' + state));
-        // window.localStorage.removeItem('state-' + state);
+        window.localStorage.removeItem('state-' + state);
         return obj;
       },
 
