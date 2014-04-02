070f480bd24c45305b85523f2e65e7c1171c224f
Anthony Kirwan <amkirwan@gmail.com>
2014-04-02 16:59:39 -0500

    update @version tag

--- src/lib/ember.oauth2.js
+++ src/lib/ember.oauth2.js
@@ -2,7 +2,7 @@
   * @overview OAuth2 library for Emberjs that stores tokens in the browsers localStorage   
   * @license   Licensed under MIT license
   *            See https://raw.github.com/amkirwan/ember-oauth2/master/LICENSE
-  * @version   0.2.4
+  * @version   0.3.0
   */
 (function() {
 /**
