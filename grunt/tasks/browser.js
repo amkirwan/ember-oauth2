e939d18bd9554af3ce154bb7fa50f6cacaebeabe
Anthony Kirwan <amkirwan@gmail.com>
2014-08-25 23:28:58 -0500

    define Ember shim for ember-oauth2 dependency

--- grunt/tasks/browser.js
+++ grunt/tasks/browser.js
@@ -1,13 +1,16 @@
 module.exports = function(grunt) {
+
   grunt.registerMultiTask('browser', "Export a module to the window", function() {
     var opts = this.options();
     this.files.forEach(function(f) {
       var output = ["(function(globals) {"];
-   
+
       output.push.apply(output, f.src.map(grunt.file.read));
-   
+
+      output.push('\n/*global define, Ember */\ndefine("ember", [], function() {\n\t"use strict";\n\treturn { "default": Ember };\n });\n');
+
       output.push(grunt.template.process(
-        'window.<%= namespace %> = requireModule("<%= barename %>");', { 
+        'window.<%= namespace %> = requireModule("<%= barename %>")["default"];', { 
         data: {
           namespace: opts.namespace,
           barename: opts.barename
