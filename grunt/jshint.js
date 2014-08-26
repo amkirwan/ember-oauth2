5d6679485e5c12794b932528cd0a7ba52c7beeb0
Anthony Kirwan <amkirwan@gmail.com>
2014-08-25 23:18:28 -0500

    move config for jshint to .jshintrc files

--- grunt/jshint.js
+++ grunt/jshint.js
@@ -1,58 +1,16 @@
 module.exports = {
-  options: {
-    node: true,
-    browser: true,
-    esnext: true,
-    bitwise: false,
-    curly: false,
-    eqeqeq: true,
-    eqnull: true,
-    immed: true,
-    latedef: true,
-    newcap: true,
-    noarg: true,
-    undef: true,
-    strict: false,
-    trailing: false,
-    smarttabs: true,
-    globals: {
-      module: true
-    },
-    gruntfile: ['gruntfile.js'],
-    lib: {
-      options: {
-        globals: {
-          jQuery: true,
-          Ember: true
-        }
-      },
-      files: {
-        src: ['lib/**/*.js']
-      }
-    },
-    spec: {
-      options: {
-        globals: {
-          describe: true,
-          beforeEach: true,
-          afterEach: true,
-          App: true,
-          it: true,
-          expect: true,
-          sinon: true,
-          Ember: true,
-          $: true
-        }
-      },
-      files: {
-        src: ['tests/*.spec.js']
-      }
-    }
+  gruntfile: ['gruntfile.js'],
+  options: { jshintrc: '.jshintrc'},
+  lib: {
+    src: ['lib/**/*.js'],
+    options: { jshintrc: '.jshintrc'}
+  },
+  tests: {
+    src: ['tests/**/*.spec.js'],
+    options: { jshintrc: 'tests/.jshintrc'}
   },
   dist: {
-    options: {
-      globals: {
-      },
-    }
+    src: ['dist/ember-oauth2.js', 'dist/ember-oauth2.amd.js'],
+    options: { jshintrc: '.jshintrc'}
   }
 }
