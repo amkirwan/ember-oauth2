05e2212842fa9620882717cc4642cc5d51e60d85
Anthony Kirwan <amkirwan@gmail.com>
2014-08-25 23:29:29 -0500

    grunt tasks for building and testing amd and global script

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,8 +11,11 @@ module.exports = function(grunt) {
   config.src = 'src/spec';
   config.spec = 'src/spec';
 
-  grunt.registerTask('default', ['jshint', 'transpile:lib', 'concat', 'browser', 'uglify', 'testem']);
-  grunt.registerTask('build', ['default']);
+  grunt.registerTask('default', ['build', 'jshint:tests', 'preprocess:amd', 'testem']);
+  grunt.registerTask('build', ['clean', 'jshint:lib', 'transpile:lib', 'concat', 'browser', 'uglify', 'jshint:dist']);
+
+  grunt.registerTask('test-amd', ['default']);
+  grunt.registerTask('test-global', ['build', 'jshint:tests', 'preprocess:global', 'testem']);
 
   grunt.registerTask('release:patch', ['jshint', 'versioner:bumpOnly:default:patch', 'build', 'jasmine', 'versioner:commitOnly:default']);
   grunt.registerTask('release:minor', ['jshint', 'versioner:bumpOnly:default:minor', 'build', 'jasmine', 'versioner:commitOnly:default']);
