ee5871275cd1fdf3e3948c52c44ec10ea5beb3a3
Anthony Kirwan <amkirwan@gmail.com>
2015-07-30 16:40:42 -0500

    test.html is no longer transpiled so can be used directly

--- grunt/testem.js
+++ grunt/testem.js
@@ -2,7 +2,7 @@ module.exports = {
   env1: {
     options: {
       "framework": "jasmine2",
-      "test_page": "tmp/result/test.html",
+      "test_page": "tests/test.html",
       "launch_in_dev": ["PhantomJS", "Chrome"],
       "launch_in_ci": ["PhantomJS"] 
     }
